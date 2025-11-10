1b12f9c69357e0dbfb64360c5a988e5072ee2393
gi0o0rgi <tsereteli.g@gmail.com>
2025-11-09 22:03:00 -0500

    Create UserDao.java

---
+++ src/main/java/edu/gmu/cs321/UserDao.java
@@ -0,0 +1,25 @@
+import edu.gmu.cs321.model.User;
+import java.util.Optional;
+
+/**
+ * Interface for User data access operations.
+ *
+ * @author Giorgi
+ * @version 1.0
+ */
+public interface UserDao {
+    /**
+     * Finds a user by their username.
+     *
+     * @param username The username to search for.
+     * @return An Optional containing the User if found, otherwise empty.
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Saves a new or existing user.
+     *
+     * @param user The User object to save.
+     */
+    void save(User user);
+}
