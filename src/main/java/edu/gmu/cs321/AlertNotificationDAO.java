ca5a184c23601ce6b7a61aef00d4dadd78795f71
gi0o0rgi <tsereteli.g@gmail.com>
2025-11-09 22:04:09 -0500

    Update AlertNotificationDAO.java

--- src/main/java/edu/gmu/cs321/AlertNotificationDAO.java
+++ src/main/java/edu/gmu/cs321/AlertNotificationDAO.java
@@ -1,8 +1,27 @@
-package edu.gmu.cs321;
+package edu.gmu.cs321.dao;
 
+import edu.gmu.cs321.model.Notification;
 import java.util.List;
 
-public interface AlertNotificationDAO {
-    void saveNotification(Notification notification);
-    List<Notification> listNotifications();
+/**
+ * Interface for Notification data access operations.
+ *
+ * @author Giorgi
+ * @version 1.0
+ */
+public interface NotificationDao {
+    /**
+     * Saves a new notification to the database.
+     *
+     * @param notification The Notification object to save.
+     */
+    void save(Notification notification);
+
+    /**
+     * Finds all notifications associated with a specific alert.
+     *
+     * @param alertId The ID of the alert.
+     * @return A List of all matching Notification objects.
+     */
+    List<Notification> findByAlertId(long alertId);
 }
